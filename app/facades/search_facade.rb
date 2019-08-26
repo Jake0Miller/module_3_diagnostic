c85cca74dbb6f171c69b93e27946e3e19e210987
Jake Miller <jake0miller@gmail.com>
2019-08-26 09:47:16 -0600

    Write search facade, nrel service, and add stations to index view. Json parse not working.

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,3 +1,15 @@
 class SearchFacade
+  def stations
+    @stations ||= service.station_data#.map {|data| Station.new(data)}
+  end
 
+  private
+
+  def service
+    @_service ||= NrelService.new
+  end
+
+  def station_data
+    @_station_data ||= service.station_data
+  end
 end
--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,3 +1,24 @@
 class NrelService
-  
+  def station_data
+    get_json("/nearest.json?")
+  end
+
+  private
+
+  def conn
+    @_conn ||= Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1") do |faraday|
+      faraday.headers["api_key"] = ENV["API_KEY"]
+      faraday.headers["fuel_type"] = "LPG,ELEC"
+      faraday.headers["location"] = "80203"
+      faraday.headers["limit"] = "10"
+      faraday.headers["format"] = "JSON"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    binding.pry
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
