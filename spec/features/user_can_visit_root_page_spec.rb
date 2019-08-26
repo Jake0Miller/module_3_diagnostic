baebccd9354b003cd9d8e79115c995466da333b5
Jake Miller <jake0miller@gmail.com>
2019-08-26 09:21:15 -0600

    Write test up to clicking on locate and expecting to reach search

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 feature "User can visit root page" do
   scenario "and view navbar contents" do
     visit "/"
+
     within(".navbar") do
       expect(page).to have_content("AltFuelFinder")
       expect(page).to have_selector("input[value='Search by zip...']")
     end
+
+    fill_in :q, with: 80203
+
+    click_on "Locate"
+
+    expect(current_path).to eq("/search")
   end
 end
