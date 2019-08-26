845c35f1e306f097de6fd06f770b644c94069470
Jake Miller <jake0miller@gmail.com>
2019-08-26 09:24:18 -0600

    Write test expecting to see 10 stations

--- spec/features/user_can_visit_root_page_spec.rb
+++ spec/features/user_can_visit_root_page_spec.rb
@@ -12,11 +12,12 @@ feature "User can visit root page" do
 
   scenario "we can search by zip" do
     visit "/"
-    
+
     fill_in :q, with: 80203
 
     click_on "Locate"
 
     expect(current_path).to eq("/search")
+    expect(page).to have_css('.station', count: 10)
   end
 end
